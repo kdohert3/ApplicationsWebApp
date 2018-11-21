d8784869b9543db3d49c00450841d9c9913d82e1
DirtyMikeBeck <michaelubeck@gmail.com>
2018-11-21 17:29:13 -0500

    Added admin functions

--- src/adminPage/adminPage.php
+++ src/adminPage/adminPage.php
@@ -1,51 +1,34 @@
-<?php
-$head = <<<EOBODY
-<!doctype html>
 <html>
   <head>
-    <title>Application System</title>
     <meta charset="utf-8">
+    <title>adminPage</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+    <link href="../../bootstrap/css/bootstrap.min.css" rel="stylesheet">
+		<link type="text/css" href="../signin.css?v=1" rel="stylesheet">
   </head>
-EOBODY;
 
-$body = <<<EOBODY
-<body>
-  <div class="container-fluid">
-    <h1>Create or Remove Users</h1>
-    <form action="" method="post">
-      <div class="form-group">
-        <label for="userType">Type of User:</label>
-        <select id="userType" name="userType" class="form-control">
-          <option value=""></option>
-          <option value="Counselor">Counselor</option>
-          <option value="Administrator">Administrator</option>
-          <option value="School">School</option>
-        </select>
-      </div>
+  <body>
+    <header id="header">
+      <h1> Administrator Page</h1>
+    </header>
 
+    <center>
+    <b>Would you like to create or remove an administrator/counselor?</b>
+    <br /> <br />
+    
+    <form action="create.php" method="post">
       <div class="form-group">
-        <label for="username">Username:</label>
-        <input type="text" id="username" name="username" class="form-control" />
-      </div>
-
-      <div class="form-group">
-        <label for"action">Action:</label>
-        <input type="radio" id="action" name="action" value="Create" />Create
-        <input type="radio" id="action" name="action" value="Remove" />Remove
-      </div>
-
-      <div class="form-group">
-        <input type="submit" value="Submit" class="form-control">
+        <input type="submit" value="Create">
       </div>
     </form>
-  </div>
-</body>
-</html>
-EOBODY;
 
-echo $head.$body;
- ?>
+    <form action="remove.php" method="post">
+      <div class="form-group">
+        <input type="submit" value="Remove">
+      </div>
+    </form>
+    </center>
+  </body>
+</html>
---
+++ src/adminPage/create.php
@@ -0,0 +1,58 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>adminPage</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="../../bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link type="text/css" href="../signin.css?v=1" rel="stylesheet">
+  </head>
+
+  <body>
+    <header id="header">
+      <h1>Administrator Page</h1>
+    </header>
+
+    <center>
+    <b>Create an Administrator or Counselor below:</b>
+    <br /> <br />
+
+
+    <form action="confirmCreate.php" method="post">
+      <div class="form-group" id="center">
+        <label for="type">Administrator or Counselor:</label><br>
+        <select id="type" name="type">
+          <option value="administrator">Administrator</option>
+          <option value="counselor">Counselor</option>
+        </select>
+      </div>
+
+      <div class="form-group">
+        <label for="username">User Name:</label><br>
+        <input type="text" id="username" name="username">
+      </div>
+
+      <div class="form-group">
+        <label for="password">Password:</label><br>
+        <input type="password" id="password" name="password">
+      </div>
+
+      <div class="form-group">
+        <label for="confirmPassword">Confirm Confirm:</label><br>
+        <input type="password" id="confirmPassword" name="cofirmPassword">
+      </div>
+
+      <div class="form-group">
+        <label for="school">School:</label><br>
+        <input type="text" id="school" name="school">
+      </div>
+
+      <div class="form-group">
+        <input type="submit" value="Create">
+      </div>
+    </form>
+
+    </center>
+  </body>
+</html>
